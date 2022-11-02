f48ec826836609d402e26ee58bdbc87b00b1a93b
FernandoLepore <juliofernandolepore@gmail.com>
2022-11-02 00:18:12 -0300

    sintaxis de las funciones flecha(arrow functions)

--- funciones/arrowFunctions(flecha).js
+++ funciones/arrowFunctions(flecha).js
@@ -26,6 +26,50 @@ let miFuncion3 = () => {
     console.log("saludo desde mi funcion flecha");
 }
 
-/* estructura aun mas simplificada de funcion flecha */
 
+/* estructura aun mas simplificada de funcion flecha */
 let miFuncion4 = () => console.log("saludo desde mi funcion flecha");
+
+
+/* estructura de la funcion flecha con return  */
+let miFuncion5 = () => {
+    return console.log("saludo desde funcion flecha 1 linea con return");
+}
+
+
+/* SINTAXIS PARA LA CREACION DE UN OBJETO(LITERAL) a traves de esta sintaxis de
+funcion flecha , en la parte de bloque se usan parentesis(sustituyen llaves) y 
+recien ahi dentro se crea al OBJETO {con llaves} */
+const objeto1 = () => ({nombre:"juan", apellido: "lopez"});
+/* aqui mando a consola el objeto y su atributo nombre */
+console.log(objeto1());
+
+
+/* estructura funcion flecha con unico parametro (simplificada) */
+let funcion8 = parametroUnico => console.log(parametroUnico);
+funcion8("saludo desde funcion6 flecha con parametros");
+
+
+/* estructura funcion flecha con parametro */
+let funcion6 = (argMensaje) => console.log(argMensaje);
+funcion6("saludo desde funcion6 flecha con parametros");
+
+
+/* estructura ordinaria de la funcion */
+let funcion7 = function (argMensaje) {
+    console.log(argMensaje);
+}
+funcion7("saludo desde funcion7 flecha con parametros");
+
+
+/* funcion flecha con varios parametros */
+const funcion9 = (num1 , num2) => num1 + num2;
+console.log(funcion9(3,8));
+
+
+/* funcion con varios parametros, en cuerpo con mas lineas */
+const funcion10 = (num1 , num2) => {
+    let resultado= num1 + num2;
+    return `el resultado es: ${resultado}`;
+}
+console.log(funcion10(8,8));
