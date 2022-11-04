675d269aadd7beec68cf1fd377d2460eb0ddb938
FernandoLepore <juliofernandolepore@gmail.com>
2022-11-04 15:23:17 -0300

    trabajando con el metodo document.write y funciones

--- domFormularios/dom.js
+++ domFormularios/dom.js
@@ -12,3 +12,24 @@ var funcion1 = ()=> {
 /* a la funcion declaro una variable local a la cual le almaceno el metodo forms 
 y le paso el id formulario, el formulario que estoy recuperando es un arreglo al
 cual voy a tener que recorrer los elementos */
+
+/* DOCUMENT WRITE  */
+/* este metodo sobreescribe todo lo que se haya cargado previamente en el html, borrando
+y reemplazando por el argumento de document.write  */
+
+var mostrar =() => document.write("adios");
+
+/* modificar elementos o cambio de contenido en el DOM HTML, a continuacion defino una
+funcion con el metodo que recoge el id manipula el texto y le asigna un nuevo contenido. */
+
+function mostrarH1 () {
+     return document
+     .getElementById("titulo")
+     .innerHTML= "este es un h1 modificado";
+}
+/* aprendiendo a manejar sintaxis de tradicional a flecha, y con el operadort +=
+se le adiciona mas contenido sin borrar el anterior como el caso siguiente. */
+
+ var mostrarH2 = () => document
+ .getElementById("titulo2")
+ .innerHTML += "<br/>he sido agregado con una funcion flecha";
